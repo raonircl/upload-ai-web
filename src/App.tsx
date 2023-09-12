79b02ed92581cc118ebf58d6265c83b391f00076
raonircl <raoni.afi@gmail.com>
2023-09-12 16:26:31 -0300

    feat: Added form to select a video

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Github } from 'lucide-react'
+import { FileVideo, Github } from 'lucide-react'
 import { Button } from "./components/ui/button"
 import { Separator } from './components/ui/separator'
 import { Textarea } from './components/ui/textarea'
@@ -41,7 +41,21 @@ export function App() {
             Lembre-se: você pode ultilizar a variável <code className='text-green-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
           </p>
         </div>
-        <aside className='w-80'></aside>
+
+        <aside className='w-80 space-y-6'>
+          <form className='space-y-6'>
+            <label 
+              htmlFor="video"
+              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-green-400'
+            >
+              <FileVideo className='w-4 h-4'/>
+              Selecione um vídeo
+            </label>
+
+            <input type="file" id='video' accept='video/mp4' className='sr-only'/>
+          </form>
+        </aside>
+
       </main>
     </div>
     )
